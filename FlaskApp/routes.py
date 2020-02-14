ca64ced38afe43f605d1053fd53516289876d5b5
Suraj Bichkunde <ssbwork39@gmail.com>
2020-02-14 11:50:19 +0530

    adds routes basic

---
+++ FlaskApp/routes.py
@@ -0,0 +1,52 @@
+from flask import render_template , url_for , flash , redirect , request , abort
+from FlaskApp import app 
+from FlaskApp.forms import LoginForm , RegistrationForm , RequestResetForm , ResetPasswordForm , InfoForm , CreatePostForm
+
+@app.route('/')
+@app.route('/home')
+def home():
+	return render_template(url_for('home.html') , form = form)
+
+@app.route('/login')
+def login():
+	form = LoginForm()
+	if form.validate_on_submit():
+		return redirect(url_for('register'))
+	return render_template('login.html',form = form)
+
+@app.route('/register')
+def register():
+	form = RegistrationForm()
+	if form.validate_on_submit():
+		return redirect(url_for('info'))
+	return render_template('register.html' , form = form)
+
+@app.route('/info')
+def info():
+	form = InfoForm()
+	if form.validate_on_submit():
+		return redirect(url_for('info'))
+	return render_template('info.html',form = form)
+
+
+@app.route('/request_reset')
+def request_reset():
+	form = RequestResetForm()
+	if form.validate_on_submit():
+		return redirect(url_for('reset_password'))
+	return render_template('request_reset.html',form = form)
+
+@app.route('/reset_password')
+def reset_password():
+	form = ResetPasswordForm()
+	if form.validate_on_submit():
+		return redirect(url_for('home'))
+	return render_template('reset_password.html')
+
+@app.route('/create_post')
+def create_post():
+	form = CreatePostForm()
+	if form.validate_on_submit():
+		return redirect(url_for('home'))
+	return render_template('post.html',form=form)
+
