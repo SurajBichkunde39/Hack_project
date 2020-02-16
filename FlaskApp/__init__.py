d2b47040aafb3e30a8d2044d5a43292905fedea6
Suraj Bichkunde <ssbwork39@gmail.com>
2020-02-16 09:21:31 +0530

    still some problems in sending mail

--- FlaskApp/__init__.py
+++ FlaskApp/__init__.py
@@ -1,7 +1,9 @@
+import os
 from flask import Flask 
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
+from flask_mail import Mail
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '32a3edcbdce951694b8cb5c14fb0ff80'
@@ -11,5 +13,12 @@ bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
 login_manager.login_view = 'login'
 login_manager.login_message_category = 'info'
+app.config['MAIL_SERVER'] = 'smtp.googlemail.com'
+app.config['MAIL_PORT'] = 587
+app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USERNAME'] = os.environ.get('MAIL_USERNAME')
+app.config['MAIL_PASSWORD'] = os.environ.get('MAIL_PASSWORD')
+mail = Mail(app)
+
 
 from FlaskApp import routes
